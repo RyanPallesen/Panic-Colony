4593aa11a70fcd74cba8d4b3475505ba94c21b4b
tarn1902 <tarn1902@live.com.au>
2021-01-29 20:48:46 +1100

    Updated and created prefabs
    Created trigger prefabs and scripts
    Updated player with locomotion

---
+++ PanicColonyProject/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour
+{
+    public UnityEvent trigEvent;
+    public string trigTag = "Player";
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == trigTag)
+		{
+			trigEvent.Invoke();
+		}
+	}
+
+	public void TestEvent()
+	{
+		Debug.Log("Triggered");
+	}
+}

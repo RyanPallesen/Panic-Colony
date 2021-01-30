08b6ffed750e346fb5e347f66749fb559ba07eeb
RyanPallesen <pallesenryan@gmail.com>
2021-01-30 23:01:12 +1100

    Working on camera stuff -RyanP

--- PanicColonyProject/Assets/Scripts/Jonathan/CameraRoomMover.cs
+++ PanicColonyProject/Assets/Scripts/Jonathan/CameraRoomMover.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CameraRoomMover : MonoBehaviour
 {
     public static CameraRoomMover instance;
+
+    public int DegreesRotatedClockwise;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +16,31 @@ public class CameraRoomMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            DegreesRotatedClockwise -= 1;
+
+            if(DegreesRotatedClockwise < 0)
+            {
+                DegreesRotatedClockwise = 3;
+            }
+            transform.Rotate(new Vector3(0,-90,0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            DegreesRotatedClockwise += 1;
+            if (DegreesRotatedClockwise > 3)
+            {
+                DegreesRotatedClockwise = 0;
+            }
+            transform.Rotate(new Vector3(0,90,0));
+        }
+
         fixedAge += Time.deltaTime;
 
-       transform.position = Vector3.Lerp(transform.position, bounds.center + Vector3.up * bounds.extents.magnitude + Vector3.back * bounds.extents.x/2, fixedAge / secondsToReach);
+        transform.position = Vector3.Lerp(transform.position, bounds.center + Vector3.up * bounds.extents.magnitude, fixedAge / secondsToReach);
     }
 
     public void SetBounds(Bounds _bounds)

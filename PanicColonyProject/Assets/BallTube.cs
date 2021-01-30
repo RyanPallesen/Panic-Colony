ff18760458398cac6758330b900b98063bc4d5cb
Aonymous <61231688+DebugTheDino@users.noreply.github.com>
2021-01-31 10:28:14 +1100

    Made changes to ball tube
    Made the offset work, as well as edited the prefabs to get them working with the script

--- PanicColonyProject/Assets/BallTube.cs
+++ PanicColonyProject/Assets/BallTube.cs
@@ -6,7 +6,7 @@ public class BallTube : MonoBehaviour
 {
     [SerializeField, Tooltip("The exit pipe")] private GameObject m_exitPipe = null;
     [SerializeField, Tooltip("The speed at which it leaves the pipe")] private float m_exitVelocity = 0.0f;
-    [SerializeField, Tooltip("The offset at which the ball comes out")] private Vector3 m_exitOffset = Vector3.zero;
+    [SerializeField, Tooltip("The offset at which the ball comes out")] private float m_exitOffset = 0.0f;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,7 +15,7 @@ public class BallTube : MonoBehaviour
         if (projectile)
         {
             projectile.velocity = m_exitPipe.transform.forward * m_exitVelocity;
-            projectile.transform.position = m_exitPipe.transform.position + Vector3.Scale(m_exitPipe.transform.forward, m_exitOffset);
+            projectile.transform.position = m_exitPipe.transform.position + (m_exitPipe.transform.forward * m_exitOffset);
         }
     }
 }

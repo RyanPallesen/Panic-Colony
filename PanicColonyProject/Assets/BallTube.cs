12b2b3ad3c6dc2a39d3df7bcd9c0aa4a824d43a0
Aonymous <61231688+DebugTheDino@users.noreply.github.com>
2021-01-31 10:37:39 +1100

    Stopped balls looping in pipes

--- PanicColonyProject/Assets/BallTube.cs
+++ PanicColonyProject/Assets/BallTube.cs
@@ -8,12 +8,15 @@ public class BallTube : MonoBehaviour
     [SerializeField, Tooltip("The speed at which it leaves the pipe")] private float m_exitVelocity = 0.0f;
     [SerializeField, Tooltip("The offset at which the ball comes out")] private float m_exitOffset = 0.0f;
 
+    private GameObject m_lastBall = null;
+
     private void OnTriggerEnter(Collider other)
     {
         Projectile projectile = other.GetComponent<Projectile>();
 
-        if (projectile)
+        if (projectile && other.gameObject != m_lastBall)
         {
+            m_lastBall = other.gameObject;
             projectile.velocity = m_exitPipe.transform.forward * m_exitVelocity;
             projectile.transform.position = m_exitPipe.transform.position + (m_exitPipe.transform.forward * m_exitOffset);
         }

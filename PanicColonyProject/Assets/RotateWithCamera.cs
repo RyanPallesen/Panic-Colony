6dd866d227ddc03ddf05db0e823c1c29823d26ee
Aonymous <61231688+DebugTheDino@users.noreply.github.com>
2021-01-31 11:38:32 +1100

    Rotation for sprites when camera moves
    Added a new script, and also fixed a few programming things

---
+++ PanicColonyProject/Assets/RotateWithCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWithCamera : MonoBehaviour
+{
+    private float m_startingRotationX = 0.0f;
+
+    private void Start()
+    {
+        m_startingRotationX = transform.eulerAngles.x;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.right, -m_startingRotationX);
+            transform.Rotate(Vector3.up, 90);
+            transform.Rotate(Vector3.right, m_startingRotationX);
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            transform.Rotate(Vector3.right, -m_startingRotationX);
+            transform.Rotate(Vector3.up, -90);
+            transform.Rotate(Vector3.right, m_startingRotationX);
+        }
+    }
+}
